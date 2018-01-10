a8ad6bf16a62d22410cc6f528b9175fb5568e48b
Dan Perrera <dan@foredesign.co>
2018-01-10 15:46:12 -0500

    added nuxt

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <section class="container">
+    <div>
+      <h1 class="title">
+        baystatedesignshop
+      </h1>
+      <h2 class="subtitle">
+        Where Massachusetts designers come to talk shop.
+      </h2>
+    </div>
+  </section>
+</template>
